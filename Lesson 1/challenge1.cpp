5f7ec0299a13c5c187faf1c3e8c97a681cc041b3
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-05-31 12:21:56 -0400

    3 challenges complete

---
+++ Lesson 1/challenge1.cpp
@@ -0,0 +1,8 @@
+// Create a program that outputs "I'm going to be the best programmer in the world!"
+
+#include <iostream>
+
+int main() {
+    std::cout << "I'm going to be the best programmer in the world!\n";
+    return 0;
+}
---
+++ Lesson 1/challenge2.cpp
@@ -0,0 +1,10 @@
+// Create a program that outputs "Hello, I'm ham!" on one line,
+// then "Hello, Ham. I am Sandwich. I love you!", and finally
+// "I love you too, Sandwich!"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, I'm ham!\n" << "Hello, Ham. I am Sandwich. I love you!\n" << "I love you too, Sandwich!\n";
+    return 0;
+}
