93fea0373aa28a8b1bfed7c81bede6cdd5c20d4b
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-05-31 12:43:23 -0400

    challenge 2 done

---
+++ Lesson 2/challenge1.cpp
@@ -0,0 +1,12 @@
+// Create FIVE char, bool, int, float, AND double variables with different values.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c = 'char';
+    bool b = true;
+    int i = 1;
+    float f = 4.20;
+    double d = 420.69;
+}
---
+++ Lesson 2/challenge2.cpp
@@ -0,0 +1,15 @@
+// Enter and output the following variables and see if you can figure out what its doing:
+// unsigned shot int b = -1;
+// unsigned long long int d = -1;
+// unsigned long double d = -1;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned short int b = -1;
+    unsigned long long int d = -1;
+    unsigned long double d = -1;
+    
+    return 0;
+}
