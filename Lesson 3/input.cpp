0c37a824976a967e71f20b1667080e54eca55e0b
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-02 21:16:54 -0400

    start

---
+++ Lesson 3/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Please enter an Int: ";
+    cin >> x;
+    cout << "Thank you!" << endl;
+    
+    return 0;
+}
