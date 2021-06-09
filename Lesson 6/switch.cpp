4f76fa0c2a33154ce7b16969bd614f06867b2910
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-09 15:37:35 -0400

    switch statements

--- Lesson 6/switch.cpp
+++ Lesson 6/switch.cpp
@@ -10,8 +10,15 @@ int main() {
     switch(1) {
         // equal to case 1:
         case 1:
+        case 2:
             // if it is, return this following statement
             cout << "at 1" << endl;
+            // if it is, return this following statement
+            cout << "at 2" << endl;
+            break;
+        case 3:
+            // if it is, return this following statement
+            cout << "at 3" << endl;
     }
     
     string z;
