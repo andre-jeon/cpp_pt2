44adce379c8cad48fd6a8ad04ca8a60268f86181
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-06-16 15:18:55 -0400

    mid point

--- Lesson 11/const.cpp
+++ Lesson 11/const.cpp
@@ -6,9 +6,13 @@ int main() {
     i = 4; // ERRPR: can't change const
 
     int arr1[i];
-    int arr2[j];
-    
+    int arr2[j]; // ERROR: needs to be const
+    int* arr3 = new int[j];
 
+    const int* a;
+    int const* b;
+    int* const c = new int(4);
+    void my_Function() const;
 
     return 0;
 }
